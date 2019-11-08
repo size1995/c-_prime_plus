06ea78ea4a74d85c6332c5a02038a06f44ee5a88
ze.si_intern <ze.si_intern@deeperception.net>
2019-11-08 18:43:12 +0800

    kkk

---
+++ 4_15.cpp
@@ -0,0 +1,20 @@
+//pointer.cpp --our first pointer variable
+#include "stdafx.h"
+#include <iostream>
+int main()
+{
+	using namespace std;
+	int updates = 6;
+	int *p_updates;
+	p_updates = &updates;
+
+	cout << "values: updates = " << updates;
+	cout << ",*p_updates = " << *p_updates << endl;
+
+	cout << "Addresses: &updates = " << &updates;
+	cout << ", p_updates = " << p_updates << endl;
+
+	*p_updates = *p_updates + 1;
+	cout << "Now updates = " << updates << endl;
+	return 0;
+}
